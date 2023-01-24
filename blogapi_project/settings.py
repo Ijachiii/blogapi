d79081efb461da7fb81a1295f91468b8a60a9d29
David Audu <davidaudu1010@gmail.com>
2023-01-24 12:58:25 +0100

    serializers

--- blogapi_project/settings.py
+++ blogapi_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,9 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "restt_framework.permissions.AllowAny"
+    ]
+}
